66504eb4bd55012dc4f5396faa9ea44df4f38ac3
RachaNdombe <rachelndombe64@gmail.com>
2026-06-17 16:59:01 +0200

    Modifications

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     type: 'OAuth2',
-    user: MAIL_USER,
+    user: process.env.MAIL_USER,
     clientId: process.env.OAUTH_CLIENT_ID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
